1236f491fc293c578b8a4c649665bafd561c3bef
ranishot <ranishot@localhost.localdomain>
2024-09-03 16:29:44 -0300

    desespero

--- src/repository/database.js
+++ src/repository/database.js
@@ -3,7 +3,7 @@ import {fetchAndJSON} from "@/utils.js";
 
 async function loadMeasurements(deviceId, startDate, endDate) {
 
-    const url = new URL('https://c05f-2804-d59-8141-b500-627c-13f5-7b3d-2193.ngrok-free.app/leituras/300')
+    const url = new URL('https://cf81-2804-d59-8141-b500-627c-13f5-7b3d-2193.ngrok-free.app/leituras/300')
 
     let readings = await fetchAndJSON(url)
     let data = []
